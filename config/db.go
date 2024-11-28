bebb761e97e98cf77b375a213f0a183d1bc9d153
Bharat Prajapati <bharat@o365.siprtc.io>
2024-11-28 04:40:02 +0000

    renamte .env to .env.example

--- config/db.go
+++ config/db.go
@@ -82,7 +82,7 @@ func ConnectDB() {
 }
 
 func PostgresConnect() {
-	err := godotenv.Load(".env")
+	err := godotenv.Load(".env.example")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
